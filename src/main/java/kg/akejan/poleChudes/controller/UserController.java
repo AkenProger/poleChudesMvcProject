9d03a7b2c57705534e818fff10d37cba05fe4279
AkenProger <meerbek11@mail.ru>
2021-11-23 12:10:50 +0300

    added styles and random money generator

--- src/main/java/kg/akejan/poleChudes/controller/UserController.java
+++ src/main/java/kg/akejan/poleChudes/controller/UserController.java
@@ -1,24 +1,23 @@
 package kg.akejan.poleChudes.controller;
 
 import kg.akejan.poleChudes.models.dtos.UsersDto;
+import kg.akejan.poleChudes.services.QuestionService;
 import kg.akejan.poleChudes.services.UserService;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
-
-import java.util.List;
 
 @Controller
 public class UserController {
 
-    private final UserService service;
-    public UserController(UserService service) {
-        this.service = service;
+    private final UserService userService;
+    private final QuestionService questionService;
+
+    public UserController(UserService userService, QuestionService questionService) {
+        this.userService = userService;
+        this.questionService = questionService;
     }
-
-
 
 
 }
